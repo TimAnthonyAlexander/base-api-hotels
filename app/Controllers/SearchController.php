e14988917b2b58df370611a272cd2a46a5215389
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-28 20:26:33 +0200

    Minor fixes

--- app/Controllers/SearchController.php
+++ app/Controllers/SearchController.php
@@ -24,7 +24,7 @@ class SearchController extends Controller
     {
         $search = Search::find($this->search_id);
 
-        if (!$search) {
+        if (!$search instanceof Search) {
             return JsonResponse::error('Search not found', 404);
         }
 
@@ -41,12 +41,17 @@ class SearchController extends Controller
 
         $location = Location::find($this->location_id);
 
-        if (!$location) {
+        if (!$location instanceof Location) {
             return JsonResponse::error('Location not found', 404);
         }
 
-        $search = new Search();
         $user = User::find($this->request->user['id'] ?? '');
+
+        if (!$user instanceof User) {
+            return JsonResponse::error('User not found', 404);
+        }
+
+        $search = new Search();
         $search->user = $user;
         $search->location = $location;
         $search->save();
